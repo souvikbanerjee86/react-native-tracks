d8e501a98b8bc87da8c22e8a318bb2990ff84164
Souvik Banerjee <souvik.mlindia@gmail.com>
2021-06-14 19:03:33 +0100

    Second commit

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,4 +1,4 @@
-import "../_mockLocation"
+//import "../_mockLocation"
 import React, { useContext, useCallback } from "react"
 import { StyleSheet, SafeAreaView } from "react-native"
 import { withNavigationFocus } from "react-navigation"
